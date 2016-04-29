f49a6116ddd293912f37761b16bc08bfcb8643a9
Grigori Fursin <grigori.fursin@cTuning.org>
2016-04-29 14:46:59 +0200

    unifying dependencies and adding user friendly names

---
+++ program/test-boost/test.cpp
@@ -0,0 +1,16 @@
+#include <boost/regex.hpp>
+#include <iostream>
+
+int main()
+{
+    std::string line;
+    boost::regex pat( "^Subject: (Re: |Aw: )*(.*)" );
+
+    while (std::cin)
+    {
+        std::getline(std::cin, line);
+        boost::smatch matches;
+        if (boost::regex_match(line, matches, pat))
+            std::cout << matches[2] << std::endl;
+    }
+}
